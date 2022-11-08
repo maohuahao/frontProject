a9ca85d837f97f1fe4b9e088efa2e02abe73d034
maohuhao <3242560235@qq.com>
2022-11-08 19:13:28 +0800

    add click event

--- project1/js/drop-down.js
+++ project1/js/drop-down.js
@@ -2,6 +2,9 @@ function dropDown() {
   let allLi = document.querySelectorAll("ul.drop-title > li");
   let allDrop = document.querySelectorAll("ul.drop-down");
 
+  /* 获取图片dom对象 */
+  let imgAll = document.querySelectorAll("ul.drop-title > li > img");
+
   for (let i = 0; i < allLi.length; i++) {
     allLi[i].onclick = function () {
       allDrop[i].style.opacity = 1;
@@ -12,13 +15,19 @@ function dropDown() {
         } else {
           allDrop[j].style.opacity = 0;
           allDrop[j].style.height = 0 + "px";
+          imgAll[j].style.transform = "rotate(0)";
+          allLi[j].style.backgroundColor = "";
         }
       }
 
       if (allDrop[i].style.height === "180px") {
         allDrop[i].style.height = 0 + "px";
+        imgAll[i].style.transform = "rotate(0)";
+        allLi[i].style.backgroundColor = "";
       } else {
         allDrop[i].style.height = 180 + "px";
+        imgAll[i].style.transform = "rotate(180deg)";
+        allLi[i].style.backgroundColor = "#fff";
       }
     };
   }
--- project1/js/mid-2.js
+++ project1/js/mid-2.js
@@ -5,54 +5,62 @@ function mid2Carousel() {
   );
 
   const allLi = ul.innerHTML;
-
   ul.innerHTML += allLi;
+  let i = 0;
 
-  let i = 1;
-
+  /* 过渡结束监听 */
   function handle() {
     ul.style.transform = "translateX(0)";
     ul.style.transition = "transform 0ms ease-in-out 0s";
+  }
+
+  /* 分页函数 */
+  function pagination(direction) {
+    if (direction == "right") {
+      if (i === 5) {
+        dotLi[i].classList.remove("dot");
+        dotLi[0].classList.add("dot");
+      } else {
+        dotLi[i].classList.remove("dot");
+        dotLi[i + 1].classList.add("dot");
+      }
+    } else {
+      if (i === 6) {
+        dotLi[0].classList.remove("dot");
+        dotLi[i - 1].classList.add("dot");
+      } else {
+        dotLi[i].classList.remove("dot");
+        dotLi[i - 1].classList.add("dot");
+      }
+    }
   }
 
   function budle() {
     ul.removeEventListener("transitionend", handle, false);
 
     ul.style.transition = "transform 500ms ease-in-out 0s";
-    ul.style.transform = `translateX(${-296 * i}px)`;
+    ul.style.transform = `translateX(${-296 * (i + 1)}px)`;
 
+    pagination("right");
+    i++;
     if (i == 6) {
       i = 0;
       ul.addEventListener("transitionend", handle, false);
     }
-
-    if (i === 6) {
-      dotLi[i - 1].classList.remove("dot");
-      dotLi[0].classList.add("dot");
-    } else if (i === 0) {
-      dotLi[i].classList.add("dot");
-      dotLi[5].classList.remove("dot");
-    } else {
-      dotLi[i - 1].classList.remove("dot");
-      dotLi[i].classList.add("dot");
-    }
-
-    i++;
   }
 
+  /* 轮播 */
   let timer = setInterval(budle, 3000);
 
-  /* */
-
-  let left = document.querySelector(".z-left");
+  /* 点击左右切图 */
   let right = document.querySelector(".z-right");
 
-  console.log(left);
-
+  /* 鼠标移入函数（清除定时器） */
   function mouseOver() {
     clearInterval(timer);
   }
 
+  /* 鼠标移出函数（启动定时器） */
   function mouseOut() {
     timer = setInterval(budle, 3000);
   }
@@ -71,21 +79,73 @@ function mid2Carousel() {
     };
   }
 
-  function onClick() {
+  function rightClick() {
     ul.style.transition = "transform 500ms ease-in-out 0s";
-    let transform = getComputedStyle(ul, null)["transform"];
-    let split = transform.split(",");
-    let translateX = parseInt(split[4]);
-    console.log(translateX);
+    ul.style.transform = `translateX(${-296 * (i + 1)}px)`;
 
-    ul.style.transform = `translateX(${translateX - 296}px)`;
-    if (translateX === -1776) {
+    pagination("right");
+    i++;
+    if (i === 6) {
+      ul.addEventListener("transitionend", handle, false);
+      i = 0;
+    }
+  }
+
+  function rightMouseup() {
+    ul.removeEventListener("transitionend", handle, false);
+  }
+
+  right.addEventListener("mouseover", mouseOver, false);
+  right.addEventListener("mouseout", mouseOut, false);
+  right.addEventListener("click", throttle(rightClick, 500), false);
+  right.addEventListener("mouseup", throttle(rightMouseup, 500), false);
+
+  /* 点击左箭头图片向右移动一张 */
+  let left = document.querySelector(".z-left");
+
+  function leftClick() {
+    ul.style.transition = "transform 500ms ease-in-out 100ms";
+    ul.style.transform = `translateX(${-296 * (i - 1)}px)`;
+    pagination("left");
+    i--;
+  }
+
+  function leftMouseover() {
+    let m = getComputedStyle(ul, null)["transform"];
+    let x = m.split(",")[4];
+
+    if (parseInt(x) === 0) {
       ul.style.transition = "transform 0ms ease-in-out 0s";
-      ul.style.transform = "translateX(0)";
+      ul.style.transform = "translateX(-1776px)";
+      i = 6;
     }
   }
 
   left.addEventListener("mouseover", mouseOver, false);
   left.addEventListener("mouseout", mouseOut, false);
-  left.addEventListener("click", throttle(onClick, 500), false);
+  left.addEventListener("click", throttle(leftClick, 500), false);
+  left.addEventListener("mousedown", throttle(leftMouseover, 500), false);
+
+  /* 定义分页动画 */
+
+  dotLi.forEach((item, key) => {
+    item.addEventListener(
+      "click",
+      function () {
+        clearInterval(timer);
+        for (let j = 0; j < dotLi.length; j++) {
+          if (dotLi[j].classList.contains("dot")) {
+            dotLi[j].classList.remove("dot");
+          }
+        }
+        item.classList.add("dot");
+        ul.style.transition = "transform 500ms ease-in-out 0s";
+        console.log(key);
+        ul.style.transform = `translateX(${-298 * key}px)`;
+        i = key;
+        timer = setInterval(budle, 3000);
+      },
+      false
+    );
+  });
 }
